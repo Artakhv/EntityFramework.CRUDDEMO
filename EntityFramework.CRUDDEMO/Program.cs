8e804506f995daf9afc66a3e24f9fd4830f68699
Artakhv <artak.hovhannisyan10@gmail.com>
2020-06-02 12:39:14 +0400

    Add project files.

---
+++ EntityFramework.CRUDDEMO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFramework.CRUDDEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
