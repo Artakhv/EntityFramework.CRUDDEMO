c29c5e33bae07e77ed0230235215c8841c616fcf
Artakhv <artak.hovhannisyan10@gmail.com>
2020-06-05 15:50:25 +0400

    write update delete

---
+++ EntityFramework.CRUDDEMO/IEmployeeRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.CRUDDEMO
+{
+    internal interface IEmployeeRepo
+    {
+        Employee Create(
+            string firstName,
+            string lastName,
+            string address,
+            string homePhone,
+            string cellPhone
+            );
+
+        Employee Update(Employee employee);
+
+        void Delete(Employee employee);
+    }
+}
