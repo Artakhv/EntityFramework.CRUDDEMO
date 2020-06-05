021af51abc095dad88c1af61bee81639ec5553fc
Artakhv <artak.hovhannisyan10@gmail.com>
2020-06-05 15:50:25 +0400

    write update delete

---
+++ EntityFramework.CRUDDEMO/EmployeeRepo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.CRUDDEMO
+{
+    internal class EmployeeRepo : IEmployeeRepo
+    {
+        private readonly EmplayeeContext employeeContext;
+
+        public EmployeeRepo(EmplayeeContext employeeContext)
+        {
+            this.employeeContext = employeeContext;
+        }
+
+        public Employee Create(string firstName, string lastName, string address, string homePhone, string cellPhone)
+        {
+            var response = employeeContext.Add(new Employee
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                HomePhone = homePhone,
+                CellPhone = cellPhone
+            });
+            employeeContext.SaveChanges();
+            return response.Entity;
+        }
+
+        public void Delete(Employee employee)
+        {
+            employeeContext.Remove(employee);
+            employeeContext.SaveChanges();
+        }
+
+        public Employee Update(Employee employee)
+        {
+            var response = employeeContext.Update(employee);
+            employeeContext.SaveChanges();
+            return response.Entity;
+        }
+    }
+}
--- EntityFramework.CRUDDEMO/Program.cs
+++ EntityFramework.CRUDDEMO/Program.cs
@@ -6,9 +6,19 @@ namespace EntityFramework.CRUDDEMO
     {
         static void Main(string[] args)
         {
-            var provider = new EmployeeProvider(new EmplayeeContext("Server=localhost;Database=Test;Trusted_Connection=True;"));
-            var employee = provider.Get(1);
-            Console.WriteLine($"Welcome {employee.FirstName}");
+            var context = new EmplayeeContext("Server=localhost;Database=Test;Trusted_Connection=True;");
+            var provider = new EmployeeProvider(context);
+            //var employee = provider.Get(1);
+            //Console.WriteLine($"Welcome {employee.FirstName} {employee.LastName}");
+
+            var repo = new EmployeeRepo(context);
+            repo.Create("Name", "Lastname", "Address", "212425", "26655");
+            var employee = provider.Get(2);
+            Console.WriteLine($"Welcome {employee.FirstName} {employee.LastName}");
+
+            //empoyee.FirstName = "FirstName";
+            //repo.Update(empoyee);
+            //repo.Delete(empoyee);
         }
     }
 }
